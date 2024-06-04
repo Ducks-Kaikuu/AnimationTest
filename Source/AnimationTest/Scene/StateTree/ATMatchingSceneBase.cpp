22104025f6a0af2f4e4b8e48899b9c0f5e4edf40
Satoshi_Nishimura <s104nishimura@gmail.com>
2024-06-04 11:50:17 +0900

    マッチング周りを単体でプラグイン化。

--- Source/AnimationTest/Scene/StateTree/ATMatchingSceneBase.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "AnimationTest/Scene/StateTree/ATMatchingSceneBase.h"
-
-FName UATMatchingSceneBase::MatchingSessionName = NAME_None;
